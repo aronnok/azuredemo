384eb5149241c1c44fa9e4e8f9156fffecff522c
Imtiaz Mirza <imz.mrz@gmail.com>
2019-06-01 17:59:49 -0400

    Test API from gatway ms

---
+++ microservices/gateway-ms/src/main/java/com/palebluedotstardust/Controller.java
@@ -0,0 +1,35 @@
+package com.palebluedotstardust;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.MimeTypeUtils;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+
+   
+
+    @RequestMapping("/")
+    public String index(){
+
+        return "Hello Gateway-ms";
+
+    }
+
+    @RequestMapping("/testnew")
+    public String testAPI(){
+
+        return "This is new test API from Gateway-MS!";
+
+    }
+
+    
+
+}
